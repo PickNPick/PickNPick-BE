30657ebd6742f5eca08ba56c12a7152218c961ec
5eungwon <kimseung0630@naver.com>
2025-03-31 01:26:17 +0900

    [add] 회원정보요청 & 회원정보수정 api구현

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const router = express.Router();
 const googlelogin_controller = require('../controller/googlelogin.controller')
+const getinfo_controller = require('../controller/getinfo.controller')
 const authMiddleware = require('../middleware/tokenverify')
+const updateinfo_controller = require('../controller/updateinfo.controller')
 
-
+//구글로그인 API
 router.get('/googlelogin',googlelogin_controller.googlelogin)
 router.get('/googlelogin/callback',googlelogin_controller.googlelogin_callback)
+
+
+//회원정보 API
+router.get('/getinfo',authMiddleware,getinfo_controller.getinfo)
+
+//회원정보수정 API
+router.post('/updateinfo',authMiddleware,updateinfo_controller.updateinfo)
 
 
 module.exports = router
