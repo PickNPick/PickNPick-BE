b24d579dbd94de1441d19aef626003784b7a970f
5eungwon <kimseung0630@naver.com>
2025-04-02 13:06:24 +0900

    [fix] get -> post로 수정

--- services/getinfo.service.js
+++ services/getinfo.service.js
@@ -3,7 +3,7 @@ const User = require('../models/user_model')
 
 exports.getinfo = async (req) =>{
     try{
-        const datas = await User.find({ email: req.user.email }) ;
+        const datas = await User.find({ email: req.body.email }) ;
         return datas
     }
     catch(err){
