97be31724ac8acdef8f52abed7da69bda586243b
5eungwon <kimseungwon0630@naver.com>
2025-04-03 16:02:10 +0900

    [add] /uploadimg 라우팅 추가

--- routes/routes.js
+++ routes/routes.js
@@ -6,14 +6,17 @@ const authMiddleware = require('../middleware/tokenverify')
 const updateinfo_controller = require('../controller/updateinfo.controller')
 const getmailbox_controller = require('../controller/getmailbox.controller')
 const addfriend_controller = require('../controller/addfriend.controller')
+const uploadimg_controller = require('../controller/uploadimg.controller')
+const multer = require('../middleware/multer')
+
+
+
 //구글로그인 API
 router.get('/googlelogin', googlelogin_controller.googlelogin)
 router.get('/googlelogin/callback', googlelogin_controller.googlelogin_callback)
 
 
 //회원정보 API
-
-
 router.post('/getinfo', authMiddleware, getinfo_controller.getinfo)
 
 
@@ -30,5 +33,10 @@ router.post('/addfriend', authMiddleware, addfriend_controller.addfriend)
 
 //친구목록조회
 router.post('/getfriendlist', authMiddleware, getfriendlist_controller.getfriendlist)
+
+//이미지 업로드
+router.post("/uploadimg",authMiddleware,multer,uploadimg_controller.uploadimg)
+
+
 
 module.exports = router
