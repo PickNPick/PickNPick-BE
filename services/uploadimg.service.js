a347c4c24dfb374c291ffa06aa431194527eb3eb
5eungwon <kimseung0630@naver.com>
2025-04-04 02:41:49 +0900

    [add] uploadimg (서비스) 구현

--- services/uploadimg.service.js
+++ services/uploadimg.service.js
@@ -1,10 +1,11 @@
 const { v4: uuidv4 } = require("uuid");
 const bucket = require("../middleware/firebase");
+const User = require('../models/user_model.js');
 
 exports.uploadimg = async (req,res)=>{
     try {
         const file = req.file;
-        const userId = req.user.id || req.body.userId; // tokenverify 통해 추출
+        const userId = req.user.email; // tokenverify 통해 추출
     
         const fileName = `profileImages/${userId}_${uuidv4()}`;
         const blob = bucket.file(fileName);
@@ -21,6 +22,10 @@ exports.uploadimg = async (req,res)=>{
     
         blobStream.on("finish", async () => {
           const publicUrl = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(fileName)}?alt=media`;
+          await User.updateOne(
+            { email: req.user.email },                    // 조건
+            { $set: { profileImage: publicUrl } } // 업데이트할 필드
+          );
           res.send({ imageUrl: publicUrl });
         });
     
