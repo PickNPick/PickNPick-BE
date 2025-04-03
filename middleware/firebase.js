f7c18992e02437d60b6549f94349f8560501d659
5eungwon <kimseung0630@naver.com>
2025-04-04 02:38:06 +0900

    [fix] firebase-admin 버킷 생성

--- middleware/firebase.js
+++ middleware/firebase.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service-account.json");
+const serviceAccount = require("../picknpick-3932d-firebase-adminsdk-fbsvc-95c09039f7.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: "your-project-id.appspot.com"
+  storageBucket: process.env.STORAGE_BUCKET
 });
 
 const bucket = admin.storage().bucket();
