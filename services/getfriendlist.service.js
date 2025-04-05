6549b69089f82ffec4cd66f68e9ac40bf4a7b563
jhnam <jhnamok@gmail.com>
2025-04-06 07:43:03 +0900

    [fix]친구목록불러오기

--- services/getfriendlist.service.js
+++ services/getfriendlist.service.js
@@ -1,15 +1,21 @@
-const mongoose = require('mongoose')
-const Friendlist = require('../models/friendlist_model')
+const mongoose = require('mongoose');
+const Friendlist = require('../models/friendlist_model');
 
-
-
-exports.getfriendlist = async (req) => {
+exports.getfriendlist = async (req, res) => {
     try {
-        const datas = await Friendlist.find({ useremail: req.user.email });
+        // Find the friendlist for the current user
+        const friendlist = await Friendlist.findOne({ useremail: req.user.email });
 
-        return datas
+        if (!friendlist) {
+            // If no friendlist found, return an empty friends array
+            return res.status(200).json({ friends: [] });
+        }
+
+        // Return the friends array from the friendlist document
+        return res.status(200).json({ friends: friendlist.friends });
     }
     catch (err) {
-        throw new Error(err)
+        console.error('Error in getfriendlist:', err);
+        return res.status(500).json({ message: '친구 목록을 가져오는 중 오류가 발생했습니다.' });
     }
 }
